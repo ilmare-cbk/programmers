926b088c0ac2a6027f30a81d476b3df88a99648e
ilmare-cbk <cbk104325@gmail.com>
2023-08-18 22:03:31 +0900

    n제곱 배열 자르기

---
+++ src/n제곱배열자르기/Solution.java
@@ -0,0 +1,14 @@
+package n제곱배열자르기;
+
+class Solution {
+    public int[] solution(int n, long left, long right) {
+        int[] answer = new int[(int) (right - left + 1)];
+        int idx = 0;
+        for (long i = left; i <= right; i++) {
+            answer[idx] = (int) Math.max((i / n) + 1, (i % n) + 1);
+            idx++;
+        }
+
+        return answer;
+    }
+}
