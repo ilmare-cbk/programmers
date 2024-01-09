b115871f66e6205b40c78415289ed6b168c20b8b
ilmare-cbk <cbk104325@gmail.com>
2024-01-09 21:08:29 +0900

    [백준] 11021

---
+++ src/A더하기B_7/Main.java
@@ -0,0 +1,22 @@
+package A더하기B_7;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= T; i++) {
+            String input = br.readLine();
+            int a = input.charAt(0) - '0';
+            int b = input.charAt(2) - '0';
+            sb.append("Case #").append(i).append(": ").append(a + b).append(System.lineSeparator());
+        }
+        System.out.println(sb);
+    }
+}
