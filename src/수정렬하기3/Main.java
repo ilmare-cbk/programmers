1790b90cb0bde8d4e7fbf76da65067d4ac15704a
ilmare-cbk <cbk104325@gmail.com>
2024-01-29 22:08:32 +0900

    [백준] 10989

---
+++ src/수정렬하기3/Main.java
@@ -0,0 +1,23 @@
+package 수정렬하기3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+        StringBuilder sb = new StringBuilder();
+        for (int i : arr) {
+            sb.append(i).append(System.lineSeparator());
+        }
+        System.out.println(sb);
+    }
+}
