4aee76d13a3a6221eb7c6efb13c3c2410def8aa1
ilmare-cbk <cbk104325@gmail.com>
2024-05-07 22:40:48 +0900

    25083 새싹

---
+++ src/새싹/Main.java
@@ -0,0 +1,14 @@
+package 새싹;
+
+public class Main {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("         ,r'\"7").append(System.lineSeparator())
+                .append("r`-_   ,'  ,/").append(System.lineSeparator())
+                .append(" \\. \". L_r'").append(System.lineSeparator())
+                .append("   `~\\/").append(System.lineSeparator())
+                .append("      |").append(System.lineSeparator())
+                .append("      |");
+        System.out.println(sb);
+    }
+}
