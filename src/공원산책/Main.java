44a103d33ff152b8bbcd25e96929323a404b2f2c
ilmare-cbk <cbk104325@gmail.com>
2024-05-09 23:52:44 +0900

    10171 고양이

---
+++ src/공원산책/Main.java
@@ -0,0 +1,12 @@
+package 공원산책;
+
+public class Main {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("\\    /\\").append(System.lineSeparator())
+                .append(" )  ( ')").append(System.lineSeparator())
+                .append("(  /  )").append(System.lineSeparator())
+                .append(" \\(__)|");
+        System.out.println(sb);
+    }
+}
