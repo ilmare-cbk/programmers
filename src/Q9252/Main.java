4437bf05302b18bca0cb335b66ab8ed212f8d72f
ilmare-cbk <cbk104325@gmail.com>
2025-04-02 13:36:31 +0900

    9252 LCS 2

---
+++ src/Q9252/Main.java
@@ -0,0 +1,60 @@
+package Q9252;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * LCS 2
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s1 = br.readLine();
+        String s2 = br.readLine();
+        int l1 = s1.length();
+        int l2 = s2.length();
+        int[][] dp = new int[l1 + 1][l2 + 1];
+
+        for (int i = 1; i <= l1; i++) {
+            for (int j = 1; j <= l2; j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        int i = l1;
+        int j = l2;
+        while (i > 0 && j > 0) {
+            if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                sb.append(s1.charAt(i - 1));
+                i--;
+                j--;
+            } else if (dp[i - 1][j] > dp[i][j - 1]) {
+                i--;
+            } else {
+                j--;
+            }
+        }
+
+        System.out.println(dp[l1][l2]);
+        System.out.println(sb.reverse());
+    }
+}
+/*
+ACAYKP
+CAPCAK
+4
+
+CAPCAK
+ACAYKP
+4
+
+AACAAABAA
+AAACAABAA
+8
+ */
