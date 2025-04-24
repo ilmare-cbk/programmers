3fa16bfb7b1577cd6be5e7ebd06d6b486ae48c6f
ilmare-cbk <cbk104325@gmail.com>
2025-04-24 20:56:51 +0900

    11568 민균이의 계략

---
+++ src/Q11568/Main.java
@@ -0,0 +1,52 @@
+package Q11568;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+/**
+ * 민균이의 계략
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            int key = arr[i];
+            int idx = binarySearch(key, list);
+
+            if (idx == list.size()) {
+                list.add(key);
+            } else {
+                list.set(idx, key);
+            }
+        }
+
+        System.out.println(list.size());
+    }
+
+    private static int binarySearch(int key, List<Integer> list) {
+        int low = 0, high = list.size() - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (list.get(mid) == key) return mid;
+            else if (list.get(mid) < key) low = mid + 1;
+            else high = mid - 1;
+        }
+
+        return low;
+    }
+}
