6435e4dfaf6fba8717d706730ed4a7bed0e4b157
ilmare-cbk <cbk104325@gmail.com>
2025-05-09 17:46:28 +0900

    16195 1, 2, 3 더하기 9

---
+++ src/Q16195/Main.java
@@ -0,0 +1,43 @@
+package Q16195;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 1, 2, 3 더하기 9
+ */
+public class Main {
+    private static final int MOD = 1_000_000_009;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        long[][] dp = new long[1001][1001];
+        for (int i = 1; i <= 1000; i++) {
+            dp[i][i] = 1;
+        }
+        dp[2][1] = dp[3][1] = 1;
+        dp[3][2] = dp[2][1] + dp[1][1];
+
+        for (int i = 4; i <= 1000; i++) {
+            for (int j = 2; j < 1000; j++) {
+                dp[i][j] = (dp[i - 1][j - 1] + dp[i - 2][j - 1] + dp[i - 3][j - 1]) % MOD;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (T-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+            long sum = 0;
+            for (int i = 1; i <= m; i++) {
+                sum = (sum + dp[n][i]) % MOD;
+            }
+            sb.append(sum).append(System.lineSeparator());
+        }
+        System.out.println(sb);
+    }
+}
