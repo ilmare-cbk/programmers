7fd9e135e6c2de5a7e1775cf1a8d0045861d8385
ilmare-cbk <cbk104325@gmail.com>
2025-05-22 13:16:55 +0900

    11758 CCW

---
+++ src/Q11758/Main.java
@@ -0,0 +1,33 @@
+package Q11758;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * CCW
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int x1 = Integer.parseInt(st.nextToken());
+        int y1 = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int x2 = Integer.parseInt(st.nextToken());
+        int y2 = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int x3 = Integer.parseInt(st.nextToken());
+        int y3 = Integer.parseInt(st.nextToken());
+
+        int answer = ccw(x1, y1, x2, y2, x3, y3);
+        System.out.println(Integer.compare(answer, 0));
+    }
+
+    private static int ccw(int x1, int y1, int x2, int y2, int x3, int y3) {
+        return (x2 - x1) * (y3 - y1) - (y2 - y1) * (x3 - x1);
+    }
+}
